ed1b7c80d1a2cfdd1ad1d094c06d20cfc5117253
saber <1999ms@live.fr>
2023-11-09 19:56:19 +0100

    login

--- src/login.js
+++ src/login.js
@@ -3,16 +3,35 @@ import { auth } from './firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import './Login.css'; // Import your CSS stylesheet
+import * as faceapi from 'face-api.js';
+import { getDatabase, ref as dbRef, get } from 'firebase/database';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
 
   const navigate = useNavigate();
 
   const videoRef = useRef(null);
   const [imageData, setImageData] = useState(null);
 
+  async function getFaceDescriptorFromDatabase(userUid) {
+    const database = getDatabase();
+    const userRef = dbRef(database, 'users/' + userUid);
+    try {
+      const snapshot = await get(userRef);
+      if (snapshot.exists()) {
+        const userData = snapshot.val();
+        return userData.faceDescriptor; // Assuming 'faceDescriptor' is stored in the database
+      } else {
+        throw new Error('No data available for user UID: ' + userUid);
+      }
+    } catch (error) {
+      console.error('Failed to fetch user data:', error);
+      throw error; // Re-throw the error to be handled by the caller
+    }
+  }
   const startWebcam = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -23,7 +42,13 @@ function Login() {
   };
 
   useEffect(() => {
-    startWebcam();
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+      await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+      await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+    };
+
+    loadModels().then(startWebcam());
   }, []);
 
   const takePicture = () => {
@@ -40,13 +65,47 @@ function Login() {
     e.preventDefault();
 
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      alert('Login Successful!');
-      navigate('/');
+      // Authenticate with Firebase using email and password
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userUid = userCredential.user.uid;
+
+      // Capture the image from the webcam
+      
+
+      // Wait for the image data to be set
+      if (!imageData) {
+        throw new Error('Please take a picture first.');
+      }
+
+      // Create an image element from the imageData
+      const img = new Image();
+      img.src = imageData;
+      await new Promise((resolve) => {
+        img.onload = resolve;
+      });
+
+      // Perform face detection on the image
+      const detections = await faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor();
+      if (detections) {
+        // Fetch the user's face descriptor from Firebase
+        const userFaceDescriptor = await getFaceDescriptorFromDatabase(userUid);
+
+        // Compare the descriptors
+        const distance = faceapi.euclideanDistance(detections.descriptor, userFaceDescriptor);
+        if (distance < 0.6) { // Threshold value, might need tuning
+          alert('Face recognized. Login Successful!');
+          navigate('/');
+        } else {
+          alert('Face not recognized. Please try again or use your password to log in.');
+        }
+      } else {
+        throw new Error('No face detected, please try again.');
+      }
     } catch (error) {
-      alert(error.message);
+      alert('Login failed: ' + error.message);
     }
   };
+
 
   return (
     <div className="login-container">
@@ -64,10 +123,16 @@ function Login() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-       
+
         <div className="webcam-container">
           <video ref={videoRef} autoPlay />
+          <button type="button" onClick={takePicture}>Take Picture</button>
         </div>
+        {imageData && (
+          <div className="image-preview">
+            <img src={imageData} alt="User" />
+          </div>
+        )}
         <button type="submit">Login</button>
         <div className="additional-options">
           <button
