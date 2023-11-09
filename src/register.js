7964767a3449aaa13f6b19868f1c12c385590df7
kenzakadri12 <kinknzakadiri001@gmail.com>
2023-11-09 19:52:37 +0100

    models+register

--- src/register.js
+++ src/register.js
@@ -6,11 +6,13 @@ import './Registration.css'; // Import your CSS stylesheet
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { getDatabase, ref as dbRef, set } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
+import * as faceapi from 'face-api.js';
 
 
 function Registration() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [modelsLoaded, setModelsLoaded] = useState(false);
 
   const navigate = useNavigate();
 
@@ -26,11 +28,31 @@ function Registration() {
       videoRef.current.srcObject = stream;
     } catch (error) {
       console.error('Error accessing webcam:', error);
+      alert('Error accessing webcam:'+ error.message)
     }
   };
 
   useEffect(() => {
-    startWebcam();
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+        // ... add other models here as needed
+      ])
+        .then(() => {
+          setModelsLoaded(true);
+          console.log('Face API models loaded');
+          startWebcam()
+        })
+        .catch((e) => {
+          console.error('Face API models failed to load', e);
+        });
+    };
+
+    loadModels();
   }, []);
 
   const takePicture = () => {
@@ -45,42 +67,59 @@ function Registration() {
 
   const handleRegistration = async (e) => {
     e.preventDefault();
-
+  
     try {
-      // Register the user using Firebase Authentication
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-
-      // Get the user's UID
-      const userUid = userCredential.user.uid;
-
-      // Upload the image to Firebase Storage
-      if (imageData) {
+      // Convert imageData to HTMLImageElement
+      let image = new Image();
+      image.src = imageData;
+      await new Promise((resolve) => {
+        image.onload = resolve;
+      });
+  
+      // Detect the face in the captured image
+      const detections = await faceapi.detectSingleFace(image, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor();
+      console.log("Detections:", detections);
+  
+      if (detections) {
+        // Register the user using Firebase Authentication
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const userUid = userCredential.user.uid;
+  
+        // Upload the image to Firebase Storage
         const imageBlob = await fetch(imageData).then((r) => r.blob());
         const imageFile = new File([imageBlob], 'profile.jpg', { type: 'image/jpeg' });
-
+  
         const storageRef = ref(storage, 'profile_images/' + userUid + '.jpg');
         const snapshot = await uploadBytes(storageRef, imageFile);
         const downloadURL = await getDownloadURL(snapshot.ref);
-
+  
         // Store the user's data, including the download URL, in the Realtime Database
         const database = getDatabase();
         const userRef = dbRef(database, 'users/' + userUid);
         await set(userRef, {
           email: email,
           profileImage: downloadURL,
+          faceDescriptor: detections.descriptor // This is an array of numbers representing the face
         });
-
+        console.log("Face descriptor saved for user:", userUid);
+  
         // Redirect to the home page or other actions
         alert('Registration Successful!');
         navigate('/');
+      } else {
+        console.log("No face detected!");
+        alert("No face detected!")
       }
     } catch (error) {
+      console.error("Error during registration:", error);
       alert('Error during registration: ' + error.message);
     }
   };
+  
 
   return (
     <div className="registration-container">
+      {!modelsLoaded && <p>Loading models, please wait...</p>}
       <form className="registration-form" onSubmit={handleRegistration}>
         <h2>Registration</h2>
         <input
