43f0bed7215718c0c91b158b8514e0e911d96223
kenzakadri12 <kadiri112@icloud.com>
2023-11-13 11:39:31 +0100

    bug

--- src/home.js
+++ src/home.js
@@ -1,12 +1,12 @@
-import React from 'react';  
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function home() {
+function Home() {
   return (
     <div className="home">
       <h1>Welcome to the Home Page</h1>
-      <p>This is your home page content. You can customize it as you like.</p>
     </div>
   );
 }
 
-export default home;
+export default Home;
