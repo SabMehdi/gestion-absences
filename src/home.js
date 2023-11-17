90df5993c29cd75ee72abe2a2146a274a21381cd
saber <1999ms@live.fr>
2023-11-17 23:22:11 +0100

    bugs

--- src/home.js
+++ src/home.js
@@ -5,15 +5,6 @@ function Home() {
   return (
     <div>
       <h2>Accueil</h2>
-      <Link to="/register">
-        <button>Nouveau étudiant</button>
-      </Link>
-      <Link to="/login">
-        <button>Ancien étudiant</button>
-      </Link>
-      <Link to="/login">
-        <button>Professeur</button>
-      </Link>
     </div>
   );
 }
