9a15706ceb3d0a35104fa5eb0737fbf492877011
saber <1999ms@live.fr>
2023-11-17 23:27:42 +0100

    dashboard

---
+++ src/DashBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function DashBoard() {
+  return (
+    <div>
+      <h2>DashBoard</h2>
+     
+    </div>
+  );
+}
+
+export default DashBoard;
